f716903466c93ac9d97aebb1bc0f2f25d5bccab1
Daniel Marin <danielmarin998@hotmail.com>
2021-08-06 16:24:09 -0500

    clase de debug

---
+++ src/debug/index.js
@@ -0,0 +1,12 @@
+var a = 'Hello';
+
+function hello() {
+    let b = 'Hello World';
+    const c = 'Hello World!';
+    if (true) {
+        let d = 'Hello World!!';
+        debugger // Con esta palabra reservada, podremos abrir el debugger en el buscador como por ejemplo Google Chrome.
+    }
+}
+
+hello();
